eb60e27c859c05d565b64e60b063e39fa7d714f1
Sofia Moscuzza <sofimoscuzza@hotmail.com>
2023-10-21 16:18:04 +0200

    radice

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -2,10 +2,34 @@ var express = require('express');
 var router = express.Router();
 var novedadesModel = require('../../models/novedadesModel');
 
-/* GET novedades page. */
+var util = require('util');
+var cloudinary = require('cloudinary').v2;
+const uploader = util.promisify(cloudinary.uploader.upload);
+const destroy = util.promisify(cloudinary.uploader.destroy);
+
+/* GET novedades page. listar las novedades */
 router.get('/', async function (req, res, next) {
 
-    var novedades = await novedadesModel.getNovedades()
+    var novedades = await novedadesModel.getNovedades();
+
+    novedades = novedades.map(novedad => {
+        if (novedad.img_id) {
+            const imagen = cloudinary.image(novedad.img_id, {
+                width: 80,
+                height: 80,
+                crop: 'fill'
+            });
+            return {
+                ...novedad, //titulo, subti y cuerpo
+                imagen
+            };
+        } else {
+            return {
+                ...novedad, //titulo, subti y cuerpo
+                imagen: '' //nada
+            };
+        }
+    });
 
     res.render('admin/novedades', {
         layout: 'admin/layout',
@@ -17,6 +41,12 @@ router.get('/', async function (req, res, next) {
 /*Para eliminar una novedad*/
 router.get('/eliminar/:id', async (req, res, next) => {
     const id = req.params.id;
+
+    let novedad = await novedadesModel.getNovedadById(id);
+    if (novedad.img_id) {
+        await (destroy(novedad.img_id));
+    }
+
     await novedadesModel.deleteNovedadesById(id);
     res.redirect('/admin/novedades')
 
@@ -31,8 +61,18 @@ router.get('/agregar', (req, res, next) => {
 
 router.post('/agregar', async (req, res, next) => {
     try {
+
+        var img_id = '';
+        if (req.files && Object.keys(req.files).length > 0) {
+            imagen = req.files.imagen;
+            img_id = (await uploader(imagen.tempFilePath)).public_id;
+        }
+
         if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") {
-            await novedadesModel.insertNovedad(req.body);
+            await novedadesModel.insertNovedad({
+                ...req.body, //spread titulo,subtitulo y cuerpo
+                img_id
+            });
             res.redirect('/admin/novedades')
         } else {
             res.render('admin/agregar', {
@@ -65,11 +105,29 @@ router.get('/modificar/:id', async (req, res, next) => {
 
 router.post('/modificar', async (req, res, next) => {
     try {
+
+        let img_id = req.body.img_original;
+        let borrar_img_vieja = false;
+        if (req.body.img_delete === "1") {
+            img_id = null;
+            borrar_img_vieja = true;
+        } else {
+            if (req.files && Object.keys(req.files).length > 0) {
+                imagen = req.files - imagen;
+                img_id = (await uploader(imagen.tempFilePath)).public_id;
+                borrar_img_vieja = true;
+            }
+        }
+        if (borrar_img_vieja && req.body.img_original) {
+            await (destroy(req.body.img_original));
+        }
+
         //console log(req.body.id); //para ver si trae id
         var obj = {
             titulo: req.body.titulo,
             subtitulo: req.body.subtitulo,
-            cuerpo: req.body.cuerpo
+            cuerpo: req.body.cuerpo,
+            img_id
         }
 
         console.log(obj) //para ver si trae los datos
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,32 @@ var express = require('express');
 var router = express.Router();
 var nodemailer = require('nodemailer');
 var novedadesModel = require('../models/novedadesModel');
+var cloudinary = require('cloudinary').v2;
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
 
   var novedades = await novedadesModel.getNovedades();
+
+  novedades = novedades.splice(0, 5); //seleccionamos los 5 primeros elementos del array
+
+  novedades = novedades.map(novedad => {
+    if (novedad.img_id) {
+        const imagen = cloudinary.url(novedad.img_id, {
+            width: 460,
+            crop: 'fill'
+        });
+        return {
+            ...novedad, 
+            imagen
+        };
+    } else {
+        return {
+            ...novedad, 
+            imagen: '/images/noimage.jpg' 
+        };
+    }
+    });
 
   res.render('index', {
     //title: 'Express',
